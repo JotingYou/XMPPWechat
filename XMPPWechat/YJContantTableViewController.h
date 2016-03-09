b41dd3abd5a9f3e5998e3077939a4558ca458df9
JotingYou <youjoting@gamil.com>
2016-03-09 15:45:55 +0800

    get friends information from openfire sever

---
+++ XMPPWechat/YJContantTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJContantTableViewController.h
+//  XMPPWechat
+//
+//  Created by 姚家庆 on 16/3/9.
+//  Copyright © 2016年 姚家庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YJContantTableViewController : UITableViewController
+
+@end
